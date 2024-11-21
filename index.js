28da1ae663770b0df83d2614197d4a8675b67f9e
Oheers <38732412+Oheers@users.noreply.github.com>
2024-11-21 09:25:58 +0000

    :truck: Moving all files to a /public/ folder and setting up Node.js and Express.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-window.onload = function() {
-    document.getElementById("searchButton").onclick = function () {
-        alert("This should open the search page.")
-    };
-}
-//this would be great if the search page exists. also use window.href()
+const { createServer } = require('node:http');
+const path = require("path");
+const express = require('express');
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.use(express.static('./public/'));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/ed.html'))
+})
+
+server.listen(port, () => {
+    console.log("Server Initiated.");
+})
---
+++ public/index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById("searchButton").onclick = function () {
+        alert("This should open the search page.")
+    };
+}
+//this would be great if the search page exists. also use window.href()
