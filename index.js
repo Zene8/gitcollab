0b8282cad31b41be49bf2c18714ee4fa9708e0ad
Oheers <38732412+Oheers@users.noreply.github.com>
2024-11-21 10:01:19 +0000

    :bug: Changing / path and removing unused imports and declarations.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { createServer } = require('node:http');
 const path = require("path");
 const express = require('express');
 const app = express();
@@ -6,13 +5,12 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
-const hostname = '127.0.0.1';
 const port = 3000;
 
 app.use(express.static('./public/'));
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, './public/ed.html'))
+    res.status(200).sendFile(path.resolve(__dirname, './public/index.html'))
 })
 
 server.listen(port, () => {
