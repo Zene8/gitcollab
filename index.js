6d6a4e77a3835b5f11da658139bc0a12bcc6c873
Oheers <38732412+Oheers@users.noreply.github.com>
2024-11-21 10:49:01 +0000

    :sparkles: Serving a consistent navbar across each webpage from one nav-file.html file.

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
-const port = 3000;
+const port = 3456;
 
 app.use(express.static('./public/'));
 
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, './public/index.html'))
+})
+
+app.get('/nav-header', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './nav-file.html'))
 })
 
 server.listen(port, () => {
--- public/index.js
+++ public/index.js
@@ -3,4 +3,11 @@ window.onload = function() {
         alert("This should open the search page.")
     };
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("loading")
+    fetch('../nav-header')
+        .then(response => response.text())
+        .then(text => document.getElementById('nav-container').innerHTML = text);
+});
 //this would be great if the search page exists. also use window.href()
